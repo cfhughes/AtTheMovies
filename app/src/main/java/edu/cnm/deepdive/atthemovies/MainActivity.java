ceb958f53196d6d82474502b44d269192061bfab
Chris <chris@speedycomputing.com>
2019-06-13 10:44:54 -0600

    Saving Data to database

--- app/src/main/java/edu/cnm/deepdive/atthemovies/MainActivity.java
+++ app/src/main/java/edu/cnm/deepdive/atthemovies/MainActivity.java
@@ -21,11 +21,4 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        MoviesViewModel viewModel = ViewModelProviders.of(this).get(MoviesViewModel.class);
-        viewModel.saveData(this);
-    }
 }
