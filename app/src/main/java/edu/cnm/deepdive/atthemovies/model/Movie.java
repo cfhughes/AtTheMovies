dffcf7378ac96b55cae6f343ab8ea856d8da95c6
Chris <chris@speedycomputing.com>
2019-06-13 11:58:23 -0600

    Began implementing Room Database

--- app/src/main/java/edu/cnm/deepdive/atthemovies/model/Movie.java
+++ app/src/main/java/edu/cnm/deepdive/atthemovies/model/Movie.java
@@ -2,17 +2,21 @@ package edu.cnm.deepdive.atthemovies.model;
 
 import android.util.SparseIntArray;
 import androidx.annotation.NonNull;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+import androidx.room.TypeConverter;
+import androidx.room.TypeConverters;
 
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
+@Entity
 public class Movie implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
-    public static Long last_id = 0L;
-
+    @PrimaryKey(autoGenerate = true)
     private Long id;
 
     private String title;
@@ -23,20 +27,10 @@ public class Movie implements Serializable {
         HORROR, ACTION, ROMCOM, DOCUMENTARY, ANIME, SCIFI, FANTASY
     }
 
+    @TypeConverters(GenreConverter.class)
     private Genre genre;
 
     private List<Actor> actors = new ArrayList<>();
-
-    public Movie() {
-        id = ++last_id;
-    }
-
-    public Movie(Long id){
-        if (last_id < id){
-            last_id = id;
-        }
-        this.id = id;
-    }
 
     public Long getId() {
         return id;
@@ -78,5 +72,19 @@ public class Movie implements Serializable {
     @Override
     public String toString() {
         return title + ": " + genre ;
+    }
+
+    private static class GenreConverter {
+
+        @TypeConverter
+        public static Genre stringToGenre(String value){
+            return Genre.valueOf(value);
+        }
+
+        @TypeConverter
+        public static String genreToString(Genre genre){
+            return genre.name();
+        }
+
     }
 }
